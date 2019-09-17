7080a92c3b3ddaa373a900e92e7f65386e837327
tymofii-yuzefovych <tymofii.yuzefovych@gmail.com>
2019-09-17 13:53:18 +0300

    city all

--- admin/src/app/pages/translate/translate.component.ts
+++ admin/src/app/pages/translate/translate.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CrudService} from "../../crud.service";
+import {Router} from "@angular/router";
+import {AuthService} from "../../../../../aplication/src/app/auth.service";
 
 @Component({
   selector: 'app-translate',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./translate.component.scss']
 })
 export class TranslateComponent implements OnInit {
-
-  constructor() { }
+  objectKeys = Object.keys;
+  public words;
+  public ln;
+  public lnObj = {ua: '', ru: ''};
+  public activeWord;
+  public loaded = false;
+  constructor(
+      private crud: CrudService,
+      private auth: AuthService,
+      private router: Router
+  ) { }
 
   ngOnInit() {
+    this.auth.onTranslate.subscribe((v: any) => {
+      if (v) {
+        this.words = v;
+        this.loaded = true;
+      }
+    });
+  }
+
+  addTranslate(data) {
+    this.activeWord = data;
+    this.lnObj.ua = data;
+    this.lnObj.ru = this.words[data].ru;
+  }
+  checkLang(ln) {
+    this.ln = ln;
+  }
+  setTranslate() {
+    this.crud.post('translator', this.lnObj, this.words[this.activeWord].id).then((v: any) => {
+      this.lnObj = {ua: '', ru: ''};
+      this.crud.get('translator').then((v: any) => {
+        if (v) {
+          this.words = v;
+        }
+      });
+    });
+    this.activeWord = null;
   }
 
 }
