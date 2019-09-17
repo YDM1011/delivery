790e0368499ff39f2ddd1047ab7bb1fcf8db61f8
hammer <>
2019-09-17 14:00:11 +0300

    config

--- aplication/src/environments/environment.prod.ts
+++ aplication/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   host: 'http://185.69.153.199:8000/api/',
   domain: 'http://185.69.153.199:8000',
-  ws: 'ws://185.69.153.199:6760/ws'
+  ws: 'ws://185.69.153.199:6761/ws'
 };
