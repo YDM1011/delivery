c6da08ef3a2f2b6f5127a169cc9726af6e35ff9b
hammer <>
2019-10-17 15:18:40 +0300

    config

--- beakend/app/model/model_methods/restifyMethod/product.js
+++ beakend/app/model/model_methods/restifyMethod/product.js
@@ -23,7 +23,8 @@ module.exports.preUpdate = async (req,res,next, backendApp) => {
                 if (e) return res.serverError(e);
                 if (!r) return res.notFound('Not found!1');
                 if (r) {
-                    let inc = parsePrice((r.price)*(r.verify ? req.body.count : 0 - r.count));
+                    let count = r.verify ? req.body.count : 0;
+                    let inc = parsePrice((r.price)*(count - r.count));
                     Basket.findOneAndUpdate({
                         products: {$in: r._id}
                     }, { $inc: {totalPrice:inc} }, {new:true})
