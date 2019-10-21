d747d65ebe2d2d12e3731982709c02804b439314
tymofii-yuzefovych <tymofii.yuzefovych@gmail.com>
2019-10-21 16:55:45 +0300

    some changesда

--- provider/src/app/components/upload/upload.component.ts
+++ provider/src/app/components/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output} from '@angular/core';
 import {DialogComponent} from "./dialog/dialog.component";
 import {MatDialog} from "@angular/material";
 import {UploadService} from "./upload.service";
@@ -9,7 +9,7 @@ import {CrudService} from "../../crud.service";
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.scss']
 })
-export class UploadComponent implements OnInit, OnDestroy {
+export class UploadComponent implements OnInit, OnDestroy, OnChanges {
   @Output() onFs = new EventEmitter();
   @Output() onCrop = new EventEmitter();
   @Input() multiple = true;
@@ -28,16 +28,16 @@ export class UploadComponent implements OnInit, OnDestroy {
         if (this.cropper) {
           // this.crud.post('upload2', {body: v}).then((v: any) => {
           //   if (!v) return;
-            this.uploadService.setFile(v)
+            this.uploadService.setFile(v);
           // }).catch(e => console.log(e));
         }
       }
     });
-    this.uploadService.onCrop.subscribe(v=>{
-      if(v){
+    this.uploadService.onCrop.subscribe(v => {
+      if (v) {
         this.onCrop.emit(v);
       }
-    })
+    });
   }
 
   ngOnChanges() {
