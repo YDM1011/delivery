624481f9081122038238e440668315fc32112417
tymofii-yuzefovych <tymofii.yuzefovych@gmail.com>
2019-11-07 14:16:32 +0200

    some changes

--- provider/src/app/pages/dashboard/dashboard.component.ts
+++ provider/src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {CrudService} from '../../crud.service';
 import {AuthService} from '../../auth.service';
+import {BaseChartDirective, Color, Label} from "ng2-charts";
+import {ChartDataSets, ChartOptions} from "chart.js";
+import * as pluginAnnotations from 'chartjs-plugin-annotation';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +13,66 @@ import {AuthService} from '../../auth.service';
 export class DashboardComponent implements OnInit {
   public user;
   public loading: boolean = false;
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Выполнены' },
+    // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Отменены' },
+    { data: [0, 40, 30, 40, 30, 10, 4], label: 'Отменены', hidden: true}
+  ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      xAxes: [{}],
+      yAxes: [
+        {
+          id: 'y-axis-0',
+          position: 'left',
+        },
+        // {
+        //   id: 'y-axis-1',
+        //   position: 'right',
+        //   gridLines: {
+        //     color: 'rgba(0,0,0,0.45)',
+        //   },
+        //   ticks: {
+        //     fontColor: 'grey',
+        //   }
+        // }
+      ]
+    },
+    annotation:{}
+  };
+  public lineChartColors: Color[] = [
+    { // grey
+      backgroundColor: 'rgba(0,150,136,0.25)',
+      borderColor: 'rgba(0,131,120,0.51)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    // { // dark grey
+    //   backgroundColor: 'rgba(77,83,96,0.2)',
+    //   borderColor: 'rgba(77,83,96,1)',
+    //   pointBackgroundColor: 'rgba(77,83,96,1)',
+    //   pointBorderColor: '#fff',
+    //   pointHoverBackgroundColor: '#fff',
+    //   pointHoverBorderColor: 'rgba(77,83,96,1)'
+    // },
+    { // red
+      backgroundColor: 'rgba(255,255,255,0.5)',
+      borderColor: 'red',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  // public lineChartPlugins = [pluginAnnotations];
+  @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
   constructor(
       private crud: CrudService,
       private auth: AuthService
@@ -21,5 +84,13 @@ export class DashboardComponent implements OnInit {
       if (!v) {return; }
       this.user = Object.assign({}, v);
     });
+  }
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    // console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    // console.log(event, active);
   }
 }
