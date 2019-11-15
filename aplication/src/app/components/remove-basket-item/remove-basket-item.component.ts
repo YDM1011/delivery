0c7e3a08ff9eeb211182f7828f9ca96070e43b55
tymofii-yuzefovych <tymofii.yuzefovych@gmail.com>
2019-11-15 16:13:52 +0200

    some changes

--- aplication/src/app/components/remove-basket-item/remove-basket-item.component.ts
+++ aplication/src/app/components/remove-basket-item/remove-basket-item.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {CrudService} from '../../crud.service';
+import {AuthService} from "../../auth.service";
 
 @Component({
   selector: 'app-remove-basket-item',
@@ -7,15 +8,33 @@ import {CrudService} from '../../crud.service';
   styleUrls: ['./remove-basket-item.component.scss']
 })
 export class RemoveBasketItemComponent implements OnInit {
+  public language;
   @Input() data;
   @Output() closeRemove = new EventEmitter();
   @Output() successRemove = new EventEmitter();
-
+  public translate: {
+    t1: {
+      ru: 'Вы уверены, что хотите удалить товар?',
+      ua: 'Ви впевнені що бажаєте видалити товар?'
+    },
+    t2: {
+      ru: 'Удалить',
+      ua: 'Видалити'
+    },
+    t3: {
+      ru: 'Отмена',
+      ua: 'Відміна'
+    }
+  };
   constructor(
-      private crud: CrudService
+      private crud: CrudService,
+      private auth: AuthService
   ) { }
 
   ngOnInit() {
+    this.auth.onLanguage.subscribe((l: any) => {
+      this.language = l;
+    })
   }
   remove() {
     this.crud.delete('product', this.data.obj._id).then((v: any) => {
