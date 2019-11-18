227ca95ade05634f9edef2d670fd4cfc781691a3
tymofii-yuzefovych <tymofii.yuzefovych@gmail.com>
2019-11-18 16:08:28 +0200

    some changes

--- aplication/src/environments/environment.prod.ts
+++ aplication/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  host: 'http://optsmart.com.ua/api/',
-  domain: 'http://optsmart.com.ua',
+  host: 'https://optsmart.com.ua/api/',
+  domain: 'https://optsmart.com.ua',
   ws: 'wss://optsmart.com.ua/ws'
 };
