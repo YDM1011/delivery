8cd8eba45e4b3ec96a84fdb84ac7100a1b82f698
hammer <>
2019-11-25 14:05:00 +0200

    ёё

--- beakend/app/model/model_methods/restifyMethod/order.js
+++ beakend/app/model/model_methods/restifyMethod/order.js
@@ -68,7 +68,7 @@ const updateBrand = (req, res, next, backendApp) =>{
                         Company.findOneAndUpdate({_id: order.companyOwner}, {
                             $pull:{brands:order.brand},
                         }).exec((e,r)=>{})
-                    } else if (obj[order.brand] > 0 && !order.brands.find(element => element == order.brand)){
+                    } else if (obj[order.brand] > 0 && (!order.brands || !order.brands.find(element => element == order.brand))){
                         console.log("test",order.brands.find(element => element == order.brand))
                         Company.findOneAndUpdate({_id: order.companyOwner}, {
                             $push:{brands:order.brand},
